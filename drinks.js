0510658efd954ba9b71e19ede2cc0a8879f9722a
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-02-20 02:42:17 -0500

    person

--- drinks.js
+++ drinks.js
@@ -1,1 +1,29 @@
-import {person} from './userprof.js';
+// import {person} from './userprof.js';
+
+
+
+
+
+class drink {
+    constructor(name, volume, abv) {
+        this.name = name;
+        this.volume = volume;
+        this.abv = abv;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getVolume() {
+        return this.volume;
+    }
+
+    getAbv(){
+        return this.abv;
+    }
+
+    getTime(){
+        return person.getTime;
+    }
+}
