8a2e545888178384b3d5dcb3f2f15c8135d40234
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-02-20 10:58:16 -0500

    commented out code

--- bac.js
+++ bac.js
@@ -1,27 +1,27 @@
-let countDrinks = localStorage.getItem(num);
-let person = localStorage.getItem("person");
-let drinks = localStorage.getItem("totaldrinks");
-let elapsed, r, weight, grams, bac; 
+// let countDrinks = localStorage.getItem(num);
+// let person = localStorage.getItem("person");
+// let drinks = localStorage.getItem("totaldrinks");
+// let elapsed, r, weight, grams, bac; 
 
-if(person.sex === "female") {
-    r = 0.55;
-}
-else if (person.sex === "male") {
-    r = 0.68;
-}
+// if(person.sex === "female") {
+//     r = 0.55;
+// }
+// else if (person.sex === "male") {
+//     r = 0.68;
+// }
 
-if(countDrink === 1) {
-    grams = drinks.getItem(0).ABV * drinks.getItem(0).Volume * 0.789;
-    bac = (grams/ (person.weight * r)) * 100
-    bac = bac
+// if(countDrink === 1) {
+//     grams = drinks.getItem(0).ABV * drinks.getItem(0).Volume * 0.789;
+//     bac = (grams/ (person.weight * r)) * 100
+//     bac = bac
          
-}
-else if(countDrinks > 1) {
+// }
+// else if(countDrinks > 1) {
 
-}
+// }
 
-function calcBAC(prevDrink, time, currentDrink, ctime) {
-    grams = currentDrink.
+// function calcBAC(prevDrink, time, currentDrink, ctime) {
+//     grams = currentDrink
     
 
-}
+// }
