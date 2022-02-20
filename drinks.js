c2a176e9fa5e71d348550ffb522223cbe8cac8ab
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-02-20 11:10:59 -0500

    comments

--- drinks.js
+++ drinks.js
@@ -1,30 +1,30 @@
-let num = localStorage.getItem("numDrinks");
-let totalDrinks =  [];
-localStorage.setItem("num", num);
+// let num = localStorage.getItem("numDrinks");
+// let totalDrinks =  [];
+// localStorage.setItem("num", num);
 
-document.getElementById("bcButton").onclick = function() {
-    const d = new Date();
-    let time = d.getTime();
-    drink = {"Time": time,  "Volume": document.getElementById("Volume").value, "ABV": document.getElementById("ABV").value};
-    num++;
-    totalDrinks.push(drink);
-    localStorage.setItem("totaldrinks", JSON.stringify(totalDrinks));
+// document.getElementById("bcButton").onclick = function() {
+//     const d = new Date();
+//     let time = d.getTime();
+//     drink = {"Time": time,  "Volume": document.getElementById("Volume").value, "ABV": document.getElementById("ABV").value};
+//     num++;
+//     totalDrinks.push(drink);
+//     localStorage.setItem("totaldrinks", JSON.stringify(totalDrinks));
 
-}
+// }
 
 
-class drink {
-    constructor(time, volume, abv) {
-        this.time = time;
-        this.volume = volume;
-        this.abv = abv;
-    }
+// class drink {
+//     constructor(time, volume, abv) {
+//         this.time = time;
+//         this.volume = volume;
+//         this.abv = abv;
+//     }
 
-    getVolume() {
-        return this.volume;
-    }
+//     getVolume() {
+//         return this.volume;
+//     }
 
-    getAbv(){
-        return this.abv;
-    }
-}
+//     getAbv(){
+//         return this.abv;
+//     }
+// }
